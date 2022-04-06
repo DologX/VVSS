dfa43bfb28343132c1846344a47f6819f5e59f38
Calandrinon <georgesut@yahoo.com>
2022-04-06 17:14:30 +0300

    Add big bang integration

---
+++ src/test/java/org/example/AppTest2.java
@@ -0,0 +1,94 @@
+package org.example;
+
+import domain.Nota;
+import domain.Pair;
+import domain.Student;
+import domain.Tema;
+import org.junit.*;
+import repository.NotaRepository;
+import repository.NotaXMLRepository;
+import repository.StudentRepository;
+import repository.TemaRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest2
+{
+    private NotaValidator notaValidator;
+    private NotaRepository notaRepository;
+    private TemaValidator assignmentValidator;
+    private TemaRepository assignmentRepository;
+    private StudentValidator studentValidator;
+    private StudentRepository studentRepository;
+
+    @Before
+    public void setup() {
+        this.notaValidator = new NotaValidator();
+        this.notaRepository = new NotaRepository(notaValidator);
+        this.assignmentValidator = new TemaValidator();
+        this.assignmentRepository = new TemaRepository(assignmentValidator);
+        this.studentValidator = new StudentValidator();
+        this.studentRepository = new StudentRepository(studentValidator);
+    }
+
+    @Test
+    public void testSaveStudent_entitySavedInTheRepositoryContainer() {
+        Student testStudent = new Student("1543", "test1", 937);
+        studentRepository.save(testStudent);
+
+        Iterable<Student> students = studentRepository.findAll();
+        Stream<Student> filteredStudents = StreamSupport.stream(students.spliterator(), false)
+                .filter(student -> student.getID().equals("1543"));
+        assertEquals(1, filteredStudents.toArray().length);
+    }
+
+    @Test
+    public void testSaveAssignment_assignmentWithValidIdIsSaved() {
+        Tema assignment = new Tema("123", "ceva tema", 5, 3);
+        assignmentRepository.save(assignment);
+
+        int numberOfAssignments = StreamSupport
+                .stream(assignmentRepository.findAll().spliterator(), false)
+                .toArray().length;
+
+        assertEquals(1, numberOfAssignments);
+    }
+
+    @Test
+    public void testAddGrade_entitySavedInRepository() {
+        Nota testNota = new Nota(new Pair<>("abc", "def"), 10, 5, "ok");
+        notaRepository.save(testNota);
+
+        Iterable<Nota> grades = notaRepository.findAll();
+        assertTrue(grades instanceof Collection<?>);
+        assertEquals(1, ((Collection<?>)grades).size());
+    }
+
+
+    @Test
+    public void testBigBangIntegration() {
+        Student testStudent = new Student("1543", "test1", 937);
+        studentRepository.save(testStudent);
+        Tema assignment = new Tema("123", "ceva tema", 5, 3);
+        assignmentRepository.save(assignment);
+        Nota testNota = new Nota(new Pair<>("1543", "123"), 10, 5, "ok");
+        notaRepository.save(testNota);
+
+        Iterable<Nota> grades = notaRepository.findAll();
+        assertTrue(grades instanceof Collection<?>);
+        assertEquals(1, ((Collection<?>)grades).size());
+    }
+}
